681f828aae80913df522fa6b1c37d64cd73303da
tikubonn <7812951+Tikubonn@users.noreply.github.com>
2026-03-16 13:15:55 +0900

    update: UI_Tab クラスに対応する表示ラベルを指定する引数 label_table を追加しました。

--- src/uilib/ui/layout/ui_tab.py
+++ src/uilib/ui/layout/ui_tab.py
@@ -7,8 +7,12 @@ from collections import OrderedDict
 
 class UI_Tab (IUI):
 
-  def __init__ (self, uis:"dict[str, uilib.ui.abc.IUI]"):
+  def __init__ (
+    self, 
+    uis:"dict[str, uilib.ui.abc.IUI]",
+    label_table:dict[str, str]={}):
     self.uis = OrderedDict(uis)
+    self.label_table = label_table
 
   def get_value (self) -> "dict[str, typing.Any]":
     return {
@@ -22,7 +26,8 @@ class UI_Tab (IUI):
       content_frame.pack(fill=tkinter.X)
       built = ui.build(content_frame)
       built.pack(anchor=tkinter.W, padx=const_.PADDING_L, pady=const_.PADDING_L)
-      notebook.add(content_frame, text=key)
+      tab_text = self.label_table.get(key, key)
+      notebook.add(content_frame, text=tab_text)
     return notebook
 
   def load_from_param (self, param:"dict[str, typing.Any]"):
